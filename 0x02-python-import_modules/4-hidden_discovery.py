2eccb3b2fade8b02dceecc620e98e2ab6e291b1a
Ndifreke Ekanem <111875002+Ndifreke000@users.noreply.github.com>
2022-12-01 21:50:30 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,8 @@
-pans
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    name_list = dir(hidden_4)
+    for name in name_list:
+        if not name.startswith("__"):
+            print(name)
