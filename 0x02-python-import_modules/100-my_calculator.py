3397632fa5e698e43233f703adc89dd70bf9a28b
Ndifreke Ekanem <111875002+Ndifreke000@users.noreply.github.com>
2022-12-01 21:48:28 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,28 @@
-pans
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import calculator_1 as calc
+    import sys
+
+    ac = len(sys.argv) - 1
+    if ac != 3:
+        sys.stderr.write("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    operator = sys.argv[2]
+    if operator not in ('+', '-', '*', '/'):
+        sys.stderr.write("Unknown operator. Available operators: "
+                         "+, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if operator == '+':
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, calc.add(a, b)))
+    elif operator == '-':
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, calc.sub(a, b)))
+    elif operator == '*':
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, calc.mul(a, b)))
+    elif operator == '/':
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, calc.div(a, b)))
+    sys.exit(0)
