b11803dbaf618c710ef2d2bf319a54a8537da7e9
Ndifreke Ekanem <111875002+Ndifreke000@users.noreply.github.com>
2022-12-05 01:18:53 +0100

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,10 @@
-vim
+#!/usr/bin/python3
+# 8-multiple_returns.py
+# black
+
+
+def multiple_returns(sentence):
+    """Returns the length of a string and its first character."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
