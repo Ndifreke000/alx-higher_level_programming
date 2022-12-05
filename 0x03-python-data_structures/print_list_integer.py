830062ef62b4d484182fadc18c4e71f92f96a8e4
Ndifreke Ekanem <111875002+Ndifreke000@users.noreply.github.com>
2022-12-05 01:23:09 +0100

    Update print_list_integer.py

--- 0x03-python-data_structures/print_list_integer.py
+++ 0x03-python-data_structures/print_list_integer.py
@@ -1,1 +1,8 @@
-vim
+#!/usr/bin/python3
+# print_list_integer.py
+# yin
+
+def print_list_integer(my_list=[]):
+    """Print all integers of a list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
