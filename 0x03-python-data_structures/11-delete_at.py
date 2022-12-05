beb9b647ff837dbfdb7c4b366f10bd2d6f146e2b
Ndifreke Ekanem <111875002+Ndifreke000@users.noreply.github.com>
2022-12-05 01:16:51 +0100

    Update 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,6 @@
 # im boredddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 
 
-
 def delete_at(my_list=[], idx=0):
     """Delete an item at a specific position in a list."""
     if idx >= 0 and idx < len(my_list):
