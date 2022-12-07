666b67f8342956827fc590760916365bf92dba0c
Ndifreke Ekanem <111875002+Ndifreke000@users.noreply.github.com>
2022-12-07 15:22:19 +0100

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,9 @@
-typo
+#!/usr/bin/python3
+# 8-simple_delete.py
+# vis
+
+
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return (a_dictionary)
