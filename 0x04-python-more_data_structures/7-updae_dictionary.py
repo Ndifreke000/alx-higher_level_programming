4636546bb5fcfcf8b5f2e9b5f4258393374a4a35
Ndifreke Ekanem <111875002+Ndifreke000@users.noreply.github.com>
2022-12-07 15:18:36 +0100

    Update 7-updae_dictionary.py

--- 0x04-python-more_data_structures/7-updae_dictionary.py
+++ 0x04-python-more_data_structures/7-updae_dictionary.py
@@ -1,1 +1,9 @@
-typo
+#!/usr/bin/python3
+# 7-update_dictionary.py
+# watch
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
