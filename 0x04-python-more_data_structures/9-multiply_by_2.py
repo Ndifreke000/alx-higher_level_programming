168364d3d271514a6b26de04455d2fe3ea76dfb1
Ndifreke Ekanem <111875002+Ndifreke000@users.noreply.github.com>
2022-12-07 15:22:58 +0100

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,8 @@
-typo
+#!/usr/bin/python3
+# 9-multiple_by_2.py
+# wiz
+
+
+def multiply_by_2(a_dictionary):
+    """Return a new dictionary with all values multipled by 2."""
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
