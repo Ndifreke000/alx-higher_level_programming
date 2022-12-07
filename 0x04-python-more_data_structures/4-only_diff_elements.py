e8f18ba37c5f703d15f47c7a0027fefe6dd3642f
Ndifreke Ekanem <111875002+Ndifreke000@users.noreply.github.com>
2022-12-07 15:14:46 +0100

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,8 @@
-typo
+#!/usr/bin/python3
+# 4-only_diff_elements.py
+# survival
+
+
+def only_diff_elements(set_1, set_2):
+    """Return a set of all elements present in only one set."""
+    return (set_1 ^ set_2)
