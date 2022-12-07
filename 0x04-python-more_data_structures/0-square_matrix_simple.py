d8c280515d8c24b99701c285ff853649ccc2363b
Ndifreke Ekanem <111875002+Ndifreke000@users.noreply.github.com>
2022-12-07 14:59:02 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,8 @@
-typo
+#!/usr/bin/python3
+# 0-square_matrix_simple.py
+# Ndifreke
+
+
+def square_matrix_simple(matrix=[]):
+    """Compute the square value of all integers of a matrix."""
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
